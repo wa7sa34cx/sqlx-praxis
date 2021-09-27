4070b53d915a997965769c282c264827438f1a9a
Alex Black <wa7@sa34.cx>
2021-09-27 15:56:58 +0300

    create add function

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ mod models;
 use anyhow::Result;
 use dotenv::dotenv;
 // use sqlx::sqlite::{SqlitePool, SqliteRow};
+use models::{NewPost, Post};
+use sql_builder::{quote, SqlBuilder};
 use sqlx::sqlite::SqlitePool;
 use std::env;
-use sql_builder::SqlBuilder;
-use models::Post;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,6 +17,21 @@ async fn run() -> Result<()> {
     dotenv().ok();
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
 
+    // Insert new post
+    // let post = Post {
+    //     id: 0,
+    //     title: "Good ' \' afternoon!".to_string(),
+    //     date:  "Bla-bla-car".to_string(),
+    //     published: false,
+    // };
+    // add_post(&pool, post).await?;
+
+    let post = NewPost {
+        title: "Bla\" new '\'post'",
+    };
+    add_post(&pool, post).await?;
+
+    // Get all posts from DB
     let posts = get_all_posts(&pool).await?;
 
     // let posts: Vec<Post> = posts.iter().map(|p| p.into()).collect();
@@ -35,22 +50,42 @@ async fn run() -> Result<()> {
 
 async fn get_all_posts(pool: &SqlitePool) -> Result<Vec<Post>> {
     let sql = SqlBuilder::select_from("posts")
-    // .field("id")
-    // .field("title")
-    // .field("date")
-    // .field("published")
-    .order_asc("id")
-    .sql()?;
+        // .field("id")
+        // .field("title")
+        // .field("date")
+        // .field("published")
+        .order_asc("id")
+        .sql()?;
 
     // let posts = sqlx::query_as::<_, Post>(&sql)
     // .fetch_all(pool)
     // .await?;
-    
-    let posts: Vec<Post> = sqlx::query_as(&sql)
-    .fetch_all(pool)
-    .await?;
+
+    let posts: Vec<Post> = sqlx::query_as(&sql).fetch_all(pool).await?;
 
     Ok(posts)
 
     // Ok(())
+}
+
+async fn add_post(pool: &SqlitePool, post: NewPost) -> Result<()> {
+    let sql = SqlBuilder::insert_into("posts")
+        // .field("id")
+        .field("title")
+        .field("date")
+        .field("published")
+        // .values(&[&quote(post.title), &quote(post.date), &0.to_string()])
+        // .values(&[&quote(post.title), "datetime('now')", "0"])
+        .values(&[format!(
+            "{}, {}, {}",
+            quote(post.title),
+            "datetime('now')",
+            "0"
+        )])
+        // .order_asc("id")
+        .sql()?;
+
+    sqlx::query(&sql).execute(pool).await?;
+
+    Ok(())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 // use derive_more::{Add, Display, From, Into};
 // use derive_more::{From, Into};
 // use sqlx::FromRow
@@ -11,4 +11,8 @@ pub struct Post {
     pub title: String,
     pub date: String,
     pub published: bool,
+}
+
+pub struct NewPost {
+    pub title: &'static str,
 }
